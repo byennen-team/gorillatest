dd426579e22bacf687b7861ef8c9118ed9be50cc
Lance Ennen <byennen@gmail.com>
2013-12-01 17:36:04 -0500

    design changes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,5 @@
 Autotest::Application.routes.draw do
 
-  #application
-  devise_for :users, controllers: {registrations: :registrations}
-
-  get 'dashboard', to: "dashboard#index"
-
-  resources :projects do
-    resources :features do
-      resources :scenarios do
-        resources :steps
-      end
-    end
-  end
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  #pages
-  get 'privacy', to: 'pages#privacy'
-  get 'terms', to: 'pages#terms'
-
-  #welcome pages
-  get '/tour', to: 'welcome#tour'
-  get 'pricing', to: 'welcome#pricing'
   root 'welcome#index'
 
 end
