c1a621021d8ec36c034d67f67128935948a68c8a
Jim Ray <jimiray@gmail.com>
2014-01-14 11:47:51 -0700

    don't verify authenticiy, temp fix

--- app/controllers/api/v1/steps_controller.rb
+++ app/controllers/api/v1/steps_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::StepsController < Api::V1::BaseController
 
+  skip_before_action :verify_authenticity_token
+  
   before_filter :find_project
   before_filter :find_feature
   before_filter :find_scenario
