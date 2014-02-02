8213353510886c06b0983b9e26f32d7d40cde349
Jim Ray <jimiray@gmail.com>
2014-02-02 11:44:54 -0700

    add some style to projects / scenarios / features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
       link_to link_text, link_path
     end
   end
+
+  def delete_glyph
+    Rails.logger.debug(content_tag(:span, class: "glyphicon"))
+    raw(content_tag(:span, "", {class: "glyphicon glyphicon-remove", style: "font-size: 1.8em; color: red"}))
+
+  end
+
+  def play_glyph
+    raw(content_tag(:span, "", {class: "glyphicon glyphicon-play", style: "font-size: 1.8em;"}))
+  end
+
 end
