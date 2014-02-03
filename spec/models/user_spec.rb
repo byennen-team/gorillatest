6635265f30396b578e0bfe8c37fbcc1c82a1367b
Jim Ray <jimiray@gmail.com>
2014-02-03 14:31:55 -0700

    remove company

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,4 @@
 require 'spec_helper'
 
 describe User do
-
-  describe 'on create' do
-
-    let!(:user) { create(:user) }
-
-    it "should create a company" do
-      expect(user.company.name).to eq(user.company_name)
-    end
-
-    it "sends welcome email after creation" do
-      ActionMailer::Base.deliveries.last.to.should eq [user.email]
-    end
-
-  end
 end
