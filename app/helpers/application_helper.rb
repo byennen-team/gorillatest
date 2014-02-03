1bf7637bae5fe1f832ec1201f50ba24b0b3cd5d4
Jim Ray <jimiray@gmail.com>
2014-02-03 14:03:28 -0700

    fix logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,14 +8,31 @@ module ApplicationHelper
     end
   end
 
-  def delete_glyph
-    Rails.logger.debug(content_tag(:span, class: "glyphicon"))
-    raw(content_tag(:span, "", {class: "glyphicon glyphicon-remove", style: "font-size: 1.8em; color: red"}))
+  def user_nav_link(glyph_name, text, link, link_options={}, options={})
+    link_to(user_nav_link_text(glyph_name, text), link, link_options)
+  end
+
+  def user_nav_link_text(glyph_name, text)
+    html = "#{glyph(glyph_name)}&nbsp;#{text}"
+    html.html_safe
+  end
+
+  def delete_glyph(options={})
+    glyph("remove", options.merge!({style: "font-size: 1.8em; color: red"}))
 
   end
 
-  def play_glyph
-    raw(content_tag(:span, "", {class: "glyphicon glyphicon-play", style: "font-size: 1.8em;"}))
+  def play_glyph(options={})
+    glyph("play", options.merge!({style: "font-size: 1.8em;"}))
+  end
+
+  def logout_glyph(options={})
+     glyph("log-out", options)
+  end
+
+  def glyph(glyph_name, options={})
+    html = content_tag(:span, "", {class: "glyphicon glyphicon-#{glyph_name}"}.merge!(options))
+    html.html_safe
   end
 
 end
