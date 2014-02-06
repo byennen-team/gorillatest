d4c447658b1f0b8a755404d33a9b84c9adbdecfb
Jim Ray <jimiray@gmail.com>
2014-02-06 09:29:43 -0700

    redirect to projects page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    dashboard_path
+    projects_path
     #company_path(current_user.company)
   end
 
