4d685311c2f2a3516e5663c849f78e330399661e
Jim Ray <jimiray@gmail.com>
2014-02-07 07:26:56 -0700

    add url validation

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+
+    it { should validate_presence_of :name }
+
+    context "with valid URL" do
+
+      let(:project) { Project.new({name: "test", url: "http://autotest.io"}) }
+
+      it "should be valid" do
+        expect(project.valid?).to eq(true)
+      end
+
+    end
+
+    context "with invalid URL" do
+
+      let(:project) { Project.new({name: "test", url: "autoo"}) }
+
+      it "should not be valid" do
+        expect(project.valid?).to eq(false)
+      end
+
+    end
+
+  end
+
+  describe "before_create" do
+
+    let(:project) { Project.create({name: "test", url: "http://autotest.io"}) }
+
+    it "should have an api key" do
+      expect(project.api_key.blank?).to eq(false)
+    end
+
+  end
+
 end
