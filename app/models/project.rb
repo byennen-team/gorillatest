8658f8517905b5fb07791161e2d56cd2f3dfc8e0
Jim Ray <jimiray@gmail.com>
2014-02-07 13:27:13 -0700

    check autotest-staging

--- app/models/project.rb
+++ app/models/project.rb
@@ -49,7 +49,7 @@ class Project
   # Add specs for this.
   def search_for_script
     # Can't have a hard coded script source:
-    autotest_script_source = "autotest.dev/assets/recordv2.js"
+    autotest_script_source = "autotest-staging.herokuapp.com/assets/recordv2.js"
     # Need to add a rescue, open will throw an exception if it can't open the URL
     begin
       document = Nokogiri::HTML(open(self.url, "Accept" => "text/html"))
