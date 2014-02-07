38af185c08f15f6ed78d76c9d4387566535a130d
Jim Ray <jimiray@gmail.com>
2014-02-07 07:26:27 -0700

    remove specs

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe WelcomeController do
 
   describe "GET 'index.html.haml'" do
-    it "returns http success" do
-      get 'index.html.haml'
-      response.should be_success
-    end
+    # it "returns http success" do
+    #   get 'index.html.haml'
+    #   response.should be_success
+    # end
   end
 
 end
