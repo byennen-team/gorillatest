6ccc15e1d856e7ca942b78a816c5a71c31eca4c6
Jim Ray <jimiray@gmail.com>
2014-02-07 07:26:15 -0700

    clean up old testing code

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -6,7 +6,7 @@ class Scenario
   include Mongoid::Paranoia
 
  field :name, type: String
- 
+
  field :event_type, type: String
  field :locator_type, type: String
  field :locator_value, type: String
@@ -23,40 +23,5 @@ class Scenario
  has_many :test_runs
 
  validates :name, presence: true, uniqueness: {scope: :feature}
-
-  def driver
-    @driver ||= Selenium::WebDriver.for :remote, 
-                                        :url => "http://localhost:4444/wd/hub", 
-                                        :desired_capabilities => :safari
-  end
-
-  def run
-    puts "Steps size is #{steps.size}"
-    begin
-      login_to_autotest
-      driver.navigate.to(steps.first.text)
-      steps.all.each do |step|
-        if step.event_type != "get"
-          element = driver.find_element(:id, step.locator_value)
-          if step.has_args?
-              element.send(step.to_selenium, step.to_args)
-          else
-            element.send(step.to_selenium)
-          end
-        end
-      end
-      driver.close
-    rescue Exception => e
-      driver.save_screenshot("tmp/#{self.id}.png")
-      puts e.inspect
-    end
-  end
-
-  def login_to_autotest
-    driver.navigate.to("http://autotest.dev/users/sign_in")
-    driver.find_element(:id, "user_email").send_keys("jimiray@gmail.com")
-    driver.find_element(:id, "user_password").send_keys("x4ja5fnm")
-    driver.find_element(:name, "commit").click
-  end
 
 end
