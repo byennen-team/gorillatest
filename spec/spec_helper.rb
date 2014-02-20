6efecfd258b2ed6a3f582f55436d1dc9d7094448
Jim Ray <jimiray@gmail.com>
2014-02-20 10:35:51 -0700

    move code coverage around

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+
 require 'simplecov'
 if ENV["COVERAGE"]
   SimpleCov.start do
@@ -14,6 +12,10 @@ if ENV["COVERAGE"]
     add_filter "/spec/"
   end
 end
+
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
