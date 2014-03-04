4d71e2bac7d9990d17711c3dc9a1bbd22d4fd24a
Philip Woo <jhw1202@gmail.com>
2014-03-03 20:28:58 -0700

    beta invitation controller and index to see who's requested invites

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,7 @@
+class Admin::ApplicationController < ActionController::Base
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
+
+end
