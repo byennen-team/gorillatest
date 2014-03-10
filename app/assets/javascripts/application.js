f80aee85ae8aa82e5ef9409780249eff16f1db7a
Lance Ennen <byennen@gmail.com>
2014-03-09 19:12:39 -0600

    move easing plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,9 @@
 //= require underscore
 //= require userFlow
 //= require plugins/jquery.validate.min
+//= require plugins/jquery.easing.min
 
-$(document).ready(function(){
+  $(document).ready(function(){
   var target = window.location.hash
   if(target != ""){
     $("a[href='" + target + "']").tab("show")
