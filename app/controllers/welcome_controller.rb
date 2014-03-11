5f85c6e42bfd5778195e803e5fefb89f575476ff
Lance Ennen <byennen@gmail.com>
2014-03-11 14:27:30 -0600

    redirect user back to project path if signed in and trying to access homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,8 @@ class WelcomeController < ApplicationController
   layout 'welcome'
   def index
     @beta_invitation = BetaInvitation.new
+    if user_signed_in?
+      redirect_to projects_path
+    end
   end
 end
