67326869ec34e8e64656a788ab6c4a246ee7a809
Jim Ray <jimiray@gmail.com>
2014-04-01 11:46:08 -0600

    add role to my-info

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,6 +67,7 @@ class User
   field :location, type: String
   field :first_name, type: String
   field :last_name, type: String
+  field :role, type: String
 
   has_many :created_projects, class_name: "Project"
   has_many :project_users
