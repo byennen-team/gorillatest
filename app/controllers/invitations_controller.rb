bd55ca59e75f5d556764bb812a8a5d3ad5c8600f
Philip Woo <jhw1202@gmail.com>
2014-04-01 17:25:40 -0600

    override devise source code to use updated syntax, escape utm source email on url

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -42,6 +42,14 @@ class InvitationsController < Devise::InvitationsController
 
   private
 
+  def resource_from_invitation_token
+    unless params[:invitation_token] && self.resource = resource_class.where(invitation_token: params[:invitation_token]).first
+      set_flash_message(:alert, :invitation_token_invalid)
+      redirect_to after_sign_out_path_for(resource_name)
+    end
+  end
+
+
   def split_emails
     @emails = params[:user][:email].split(",")
   end
