eb877ef79b9fe581ef64b2bcbb662c2c053ac6b0
Jim Ray <jimiray@gmail.com>
2014-04-02 17:51:36 -0600

    add link to play developer mode

--- app/helpers/scenarios_helper.rb
+++ app/helpers/scenarios_helper.rb
@@ -1,2 +1,14 @@
 module ScenariosHelper
+
+  def play_developer_mode_button(scenario)
+    url = developer_mode_url(scenario)
+    html = link_to(raw("#{glyph_button('play', "Play Developer Mode")}"), url, class: "btn btn-success btn-xs", target: "_blank")
+    return html.html_safe
+  end
+
+  def developer_mode_url(scenario)
+    join_char = scenario.steps.first.text.include?("?") ? "&" : "?"
+    "#{scenario.steps.first.text}#{join_char}developer=true&scenario_id=#{scenario.id.to_s}"
+  end
+
 end
