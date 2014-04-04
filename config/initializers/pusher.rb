dc9c97c92a26ab0fb04a8f2dbb5dd3418346d387
Philip Woo <jhw1202@gmail.com>
2014-04-04 12:05:58 -0600

    pusher dev || test env condition

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,8 @@
-require 'pusher'
-Pusher.url = ENV["PUSHER_URL"]
-Pusher.logger = Rails.logger
-Pusher.app_id = ENV["PUSHER_APP_ID"]
-Pusher.key = ENV["PUSHER_KEY"]
-Pusher.secret = ENV["PUSHER_SECRET"]
+if Rails.env.development? || Rails.env.test?
+  require 'pusher'
+  Pusher.url = ENV["PUSHER_URL"]
+  Pusher.logger = Rails.logger
+  Pusher.app_id = ENV["PUSHER_APP_ID"]
+  Pusher.key = ENV["PUSHER_KEY"]
+  Pusher.secret = ENV["PUSHER_SECRET"]
+end
