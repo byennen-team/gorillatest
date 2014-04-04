10f9a323f63ea202b03ee107d1ae034aa0dbed44
Jim Ray <jimiray@gmail.com>
2014-04-03 19:54:51 -0600

    add projects to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,7 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @projects = current_user.projects
     #Rails.logger.debug("user is #{current_user.inspect}")
     #Rails.logger.debug("current company is #{current_company.inspect}")
     #Rails.logger.debug("current company projects are #{current_company.projects.size}")
