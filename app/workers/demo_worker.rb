cb16812099c6d3c1bbdbe6135ca1fc67e8d33de5
Philip Woo <jhw1202@gmail.com>
2014-06-10 16:23:41 -0600

    time and status updating for fake playback

--- app/workers/demo_worker.rb
+++ app/workers/demo_worker.rb
@@ -6,6 +6,7 @@ class DemoWorker
     test_run = ScenarioTestRun.find(test_run_id)
     browser_test = test_run.browser_tests.find(browser_test_id)
     scenario = test_run.scenario
+    status = nil
 
     browser_test.initialize_test_history
 
@@ -15,11 +16,15 @@ class DemoWorker
         browser_test.save_history(step, step.to_s, "fail")
         # browser_test.update_attribute(:screenshot_filename, )
         Pusher.trigger(browser_test.channel_name, "step_pass", {status: "fail", text:"#{step.to_s}", scenario_id: scenario.id.to_s})
+        status = "fail"
         break
       else
         browser_test.save_history(step, step.to_s, "pass")
         Pusher.trigger(browser_test.channel_name, "step_pass", {status: "pass", text:"#{step.to_s}", scenario_id: scenario.id.to_s})
       end
     end
+
+    browser_test.update_attribute(:status, status || "pass")
+    test_run.reload.complete
   end
 end
