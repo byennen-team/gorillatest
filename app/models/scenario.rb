888cec3cb2231cdb83ec1f83ad789960a92f80ed
Philip Woo <jhw1202@gmail.com>
2014-06-12 15:55:28 -0600

    adds timestamp to scenario, dont include project test runs that happened before a scenario was created

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -5,6 +5,7 @@ class Scenario
   include Mongoid::Document
   include Mongoid::Paranoia
   include Mongoid::Slug
+  include Mongoid::Timestamps
 
   field :name, type: String
   field :window_x, type: Integer
@@ -36,7 +37,7 @@ class Scenario
   end
 
   def all_test_runs
-    (test_runs + project.test_runs).sort_by(&:ran_at).reverse
+    (test_runs + project.test_runs).select{|run| run.created_at > (created_at || steps.first.created_at)}.sort_by(&:ran_at).reverse
   end
 
 end
