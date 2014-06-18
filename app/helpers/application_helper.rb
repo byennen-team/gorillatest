9cb4fe486aeb091ad38bf8f2701514bdbbe1d6a5
Lance Ennen <byennen@gmail.com>
2014-06-17 23:13:09 -0500

    navbar active and hover updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path, class: "#{class_name}"
+    end
+  end
+
   #add active class to links
-  def nav_link(link_text, link_path, italic_class=nil)
+  def icon_link(link_text, link_path, italic_class=nil)
     class_name = current_page?(link_path) ? 'active' : ''
     if link_text == "Projects" && projects_controller?(params[:controller])
       class_name = 'active'
