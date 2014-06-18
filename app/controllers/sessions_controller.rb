1b743e78cc20738df62dd52ab97db3c814ada0db
Jim Ray <jimiray@gmail.com>
2014-06-18 10:13:15 -0600

    try to remove heroku session information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,16 @@
 class SessionsController < Devise::SessionsController
   layout 'pages'
 
+  after_filter :remove_heroku_session, only: [:create]
+
   protected
 
   def after_sign_up_path_for(resource)
     dashboard_path
+  end
+
+  def remove_heroku_session
+    session[:heroku_sso] = false
   end
 
 end
