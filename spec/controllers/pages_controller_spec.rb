e74899078044a8912d5fb87fda34666d8a164160
Lance Ennen <byennen@gmail.com>
2014-06-19 15:07:18 -0500

    comment out privacy page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'privacy'" do
-    it "returns http success" do
-      get 'privacy'
-      response.should be_success
-    end
-  end
+  # describe "GET 'privacy'" do
+  #   it "returns http success" do
+  #     get 'privacy'
+  #     response.should be_success
+  #   end
+  # end
 
   describe "GET 'terms'" do
     it "returns http success" do
