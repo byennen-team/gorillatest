c9f5621af1223e2c58017641a94f699d24e8c6bb
Lance Ennen <byennen@gmail.com>
2014-06-20 15:44:33 -0500

    fix run test modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -111,7 +111,7 @@ module ApplicationHelper
     Rails.logger.debug("rendering play button")
     project = testable.is_a?(Project) ? testable : testable.project
     if project.creator.has_minutes_available?
-      html = link_to play_glyph(text: text), "javascript:void(0)", "data-target" => "#selectBrowers#{project.id}", "data-toggle" => "modal", class: "btn btn-success btn-xs"
+      html = link_to play_glyph(text: text), "javascript:void(0)", "data-target" => "#selectBrowers_#{project.id}", "data-toggle" => "modal", class: "btn btn-success btn-xs"
     else
       html = link_to play_glyph(text: text), edit_user_registration_path(anchor: "change-plan"), class: "btn btn-success btn-xs"
     end
