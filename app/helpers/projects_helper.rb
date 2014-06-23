bcff577c3774fe2a86b396d000126c206803e45b
Philip Woo <jhw1202@gmail.com>
2014-06-23 10:11:33 -0600

    mailto link on embed script display modal

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+
+  def script_email_content(project)
+    newline = "%0A"
+    subject = "subject=Please add this JS Snippet so I can start GorillaTesting!"
+    body = "body=Hello my smart friend -- I would like to start using GorillaTest to turn my testing frown upside down, and in order to do so I need you to insert the code below into the HEAD of the site to be tested. If you have any questions feel free to check out the documentation at https://www.gorillatest.com/documentation or email support@gorillatest.com."
+    script = "<script src='#{ENV['API_URL']}/assets/recordv2.js' data-api-key='#{project.api_key}' data-project-id='#{project.id.to_s}'></script>"
+
+    return "#{subject}#{newline}&#{body}#{newline}#{newline}CODE:#{newline}#{script}"
+  end
 end
