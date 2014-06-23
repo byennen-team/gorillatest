1da89891bcbffba3480d6a2da9e4d5bf1c6303b4
Lance Ennen <byennen@gmail.com>
2014-06-23 18:36:14 -0500

    argument error for rails 4.1.0 - setup scope later

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,11 +8,11 @@ class DashboardController < ApplicationController
     project_ids = current_user.projects.map(&:id)
     @project_last5tests = ProjectTestRun.in({project_id: project_ids}).limit(5).order('ran_at DESC').limit(5).to_a
     scenario_ids = Scenario.in({project_id: project_ids}).map(&:id)
-    @scenario_last5tests = ScenarioTestRun.in({scenario_id: scenario_ids}).order('ran_at DESC').limit(5).to_a
+    @scenario_last5tests = ScenarioTestRun.in({scenario_id: scenario_ids}).order('ran_at DESC').to_a
     @last5_test_runs = @project_last5tests + @scenario_last5tests
     Rails.logger.debug(@project_last5tests.inspect)
     Rails.logger.debug(@scenario_last5tests.inspect)
-    @last5_test_runs.sort_by!(&:ran_at).reverse!
+    #@last5_test_runs.sort_by!(&:ran_at).reverse!
   end
 
 end
